a267b75f728aaf6ae752eef26f9fef3440889f47
Laíza Souza <laiza.souza@meupag.com.br>
2020-08-15 16:23:27 -0300

    Breeds API

---
+++ Cats/Cats/Breeds/API/BreedApiClient.swift
@@ -0,0 +1,9 @@
+//
+//  BreedApiClient.swift
+//  Cats
+//
+//  Created by Laiza Souza on 15/08/20.
+//  Copyright © 2020 cats. All rights reserved.
+//
+
+import Foundation
---
+++ Cats/Cats/Breeds/API/Models/Breeds.swift
@@ -0,0 +1,13 @@
+//
+//  Breeds.swift
+//  Cats
+//
+//  Created by Laiza Souza on 15/08/20.
+//  Copyright © 2020 cats. All rights reserved.
+//
+
+import Foundation
+
+struct BreedsRequest: Codable {
+    var name: String
+}
