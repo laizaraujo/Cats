1914456f54bae974e4dc05e6d66604e1360fde2c
Laíza Souza <laiza.souza@meupag.com.br>
2020-08-20 14:28:59 -0300

    Moving navigation layout to coordinator class for breeds list

---
+++ Cats/Cats/Core/UIPatterns/CustomActivityIndicator.swift
@@ -0,0 +1,13 @@
+//
+//  CustomLoad.swift
+//  Cats
+//
+//  Created by Laiza Souza on 16/08/20.
+//  Copyright © 2020 cats. All rights reserved.
+//
+
+import Foundation
+
+class CustomLoad {
+    
+}
