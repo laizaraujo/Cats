e18e19e57179cd999db16e19fbd751e14ee20ee7
Laíza Souza <laiza.souza@meupag.com.br>
2020-08-20 20:04:55 -0300

    Creating UIImageExtension to set image with url

---
+++ Cats/Cats/Core/Extensions/UIImageViewExtension.swift
@@ -0,0 +1,28 @@
+//
+//  UIImageView.swift
+//  Cats
+//
+//  Created by Laiza Souza on 20/08/20.
+//  Copyright © 2020 cats. All rights reserved.
+//
+
+import UIKit
+import AlamofireImage
+
+extension UIImageView {
+    func setImage(with url: URL?, placeholderImage: UIImage, errorImage: UIImage) {
+        guard let imageUrl = url else {
+            image = placeholderImage
+            return
+        }
+        
+        af.setImage(withURL: imageUrl, placeholderImage: placeholderImage, imageTransition: .crossDissolve(0.5), runImageTransitionIfCached: false) { [weak self] response in
+            switch response.result {
+                case .failure:
+                    self?.image = errorImage
+                case .success:
+                    break
+            }
+        }
+    }
+}
---
+++ Cats/Cats/Core/Utils/LanguageUtils.swift
@@ -0,0 +1,9 @@
+//
+//  LanguageUtils.swift
+//  Cats
+//
+//  Created by Laiza Souza on 20/08/20.
+//  Copyright © 2020 cats. All rights reserved.
+//
+
+import Foundation
