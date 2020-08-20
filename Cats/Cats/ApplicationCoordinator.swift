bb2aac12220a45b29ec172120b012213c0cff1ce
Laíza Souza <laiza.souza@meupag.com.br>
2020-08-20 20:18:57 -0300

    Creating Application coordinator, set windom at AppDelegate with Coordinator

--- Cats/Cats/ApplicationCoordinator.swift
+++ Cats/Cats/ApplicationCoordinator.swift
@@ -1,9 +1,31 @@
 //
-//  AplicationCoordinator.swift
+//  ApplicationCoordinator.swift
 //  Cats
 //
 //  Created by Laiza Souza on 16/08/20.
 //  Copyright © 2020 cats. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class ApplicationCoordinator: Coordinator {
+    private let window: UIWindow
+    private let rootViewController: UINavigationController
+    private var firstSceneCoordinator: BreedsListCoordinator
+    
+    init(window: UIWindow) {
+        self.window = window
+        self.rootViewController = UINavigationController()
+        self.firstSceneCoordinator = BreedsListCoordinator(presenter: rootViewController)
+    }
+    
+    func start() {
+        window.rootViewController = rootViewController
+        firstSceneCoordinator.start()
+        window.makeKeyAndVisible()
+    }
+    
+    func popView() {
+        
+    }
+}
